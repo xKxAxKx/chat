c1c185d43aeec3768a57028eeacd5a10e707ee10
xKxAxKx <pekepooooong@gmail.com>
2020-05-17 21:05:50 +0900

    コメントの追加

--- client.go
+++ client.go
@@ -16,6 +16,11 @@ type client struct {
   room *room
 }
 
+/*
+  clientがWebSocketからReadMessageを使ってデータを読み込むために使う
+  受け取ったメッセージはすぐにroomのforwardチャネルに送られる
+  何かしらのエラーが起こった場合、ループから抜けてWebSocketを閉じる
+*/
 func (c *client) read() {
   for {
     if _, msg, err := c.socket.ReadMessage(); err ==nil {
@@ -27,6 +32,11 @@ func (c *client) read() {
   c.socket.Close()
 }
 
+/*
+  継続的にsendチャネルからメッセージを受け取る
+  WebSocketのWriteMessageメソッドを使って書き出す
+  書き込みが失敗するとbreakでforから抜けて、WebSocketを閉じる
+*/
 func (c *client) write() {
   for msg := range c.send {
     if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
