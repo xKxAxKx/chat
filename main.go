597328a4be0f05515e386972e4aef56fa4cb9e54
xKxAxKx <pekepooooong@gmail.com>
2020-05-17 17:02:15 +0900

    コメント追加

--- main.go
+++ main.go
@@ -3,7 +3,29 @@ package main
 import (
   "log"
   "net/http"
+  "text/template"
+  "path/filepath"
+  "sync"
 )
+
+// templは1つのテンプレートを表す
+type templateHandler struct {
+  once  sync.Once
+  filename string
+  templ *template.Template
+}
+
+// HTTPリクエストを処理するtemplateHandler型のメソッド
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  // t.once.Doを定義しておくと、複数のゴルーチンがServeHTTPを実行してもここは一度しか実行されない
+  t.once.Do(func() {
+    t.templ =
+      template.Must(template.ParseFiles(filepath.Join("templates",
+        t.filename)))
+  })
+  t.templ.Execute(w, nil)
+}
+
 
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
