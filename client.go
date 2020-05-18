75fcc1e5509ec2f6326a35b7a47412b098e9dfeb
xKxAxKx <pekepooooong@gmail.com>
2020-05-18 16:23:10 +0900

    goimportsによるコード整形

--- client.go
+++ client.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 )
 
 // clientはチャットを行っているユーザを指す
 type client struct {
-  // このユーザのためのWebSocket
-  socket *websocket.Conn
+	// このユーザのためのWebSocket
+	socket *websocket.Conn
 
-  // メッセージが送られるチャネル
-  send chan []byte
+	// メッセージが送られるチャネル
+	send chan []byte
 
-  // クライアントが参加しているチャットルーム
-  room *room
+	// クライアントが参加しているチャットルーム
+	room *room
 }
 
 /*
@@ -22,14 +22,14 @@ type client struct {
   何かしらのエラーが起こった場合、ループから抜けてWebSocketを閉じる
 */
 func (c *client) read() {
-  for {
-    if _, msg, err := c.socket.ReadMessage(); err ==nil {
-      c.room.forward <- msg
-    } else {
-      break
-    }
-  }
-  c.socket.Close()
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
 }
 
 /*
@@ -38,10 +38,10 @@ func (c *client) read() {
   書き込みが失敗するとbreakでforから抜けて、WebSocketを閉じる
 */
 func (c *client) write() {
-  for msg := range c.send {
-    if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
-      break
-    }
-  }
-  c.socket.Close()
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
 }
--- main.go
+++ main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "text/template"
-  "path/filepath"
-  "sync"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
 // templは1つのテンプレートを表す
 type templateHandler struct {
-  once  sync.Once
-  filename string
-  templ *template.Template
+	once     sync.Once
+	filename string
+	templ    *template.Template
 }
 
 // HTTPリクエストを処理するtemplateHandler型のメソッド
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  // t.once.Doを定義しておくと、複数のゴルーチンがServeHTTPを実行してもここは一度しか実行されない
-  t.once.Do(func() {
-    t.templ =
-      template.Must(template.ParseFiles(filepath.Join("templates",
-        t.filename)))
-  })
-  t.templ.Execute(w, nil)
+	// t.once.Doを定義しておくと、複数のゴルーチンがServeHTTPを実行してもここは一度しか実行されない
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-  // ルートパスの定義
-  http.Handle("/", &templateHandler{filename: "chat.html"})
+	// ルートパスの定義
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
-  // WEBサーバーを開始
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    log.Fatal("ListenAndServe:", err)
-  }
+	// WEBサーバーを開始
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
