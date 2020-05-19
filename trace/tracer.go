a2a694961a2fd4833439b437cf85d2c517941199
xKxAxKx <pekepooooong@gmail.com>
2020-05-19 16:23:30 +0900

    tracerの作成

--- trace/tracer.go
+++ trace/tracer.go
@@ -1,8 +1,26 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 /*
   コード内でのイベントを記録できるオブジェクトを表すインタフェース
 */
 type Tracer interface {
 	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
 }
