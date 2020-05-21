f113f0ed63e9645ad84781e6829ea93afa8269d8
xKxAxKx <pekepooooong@gmail.com>
2020-05-21 17:25:56 +0900

    GetAvatarURLを追加

--- chat/main.go
+++ chat/main.go
@@ -55,7 +55,8 @@ func main() {
 	)
 
 	// r := newRoom(UseAuthAvatar)
-	r := newRoom(UseGravatar)
+	// r := newRoom(UseGravatar)
+	r := newRoom(UseFileSystemAvatar)
 	r.tracer = trace.New(os.Stdout)
 
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
