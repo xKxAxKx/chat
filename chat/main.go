6245dda9a98042a846b4f660ee66ebd568238087
xKxAxKx <pekepooooong@gmail.com>
2020-05-21 14:14:58 +0900

    クッキーをメアドに保存&gravatarを使う

--- chat/main.go
+++ chat/main.go
@@ -54,7 +54,8 @@ func main() {
 		),
 	)
 
-	r := newRoom(UseAuthAvatar)
+	// r := newRoom(UseAuthAvatar)
+	r := newRoom(UseGravatar)
 	r.tracer = trace.New(os.Stdout)
 
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
