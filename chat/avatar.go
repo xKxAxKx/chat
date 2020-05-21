3e77640e2832e167651d8bb850315906e0f3cddd
xKxAxKx <pekepooooong@gmail.com>
2020-05-21 11:17:24 +0900

    テストの追加とAuthAvatar型の作成

--- chat/avatar.go
+++ chat/avatar.go
@@ -16,3 +16,15 @@ type Avatar interface {
 	*/
 	GetAvatarURL(c *client) (string, error)
 }
+
+type AuthAvatar struct {}
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+  if url, ok := c.userData["avatar_url"]; ok {
+    if urlStr, ok := url.(string); ok {
+      return urlStr, nil
+    }
+  }
+  return "", ErrNoAvatarURL
+}
